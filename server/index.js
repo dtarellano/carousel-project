e6ab8e644514e3f1530253b65acadf1e97f59786
David Arelano <dtarellano1@gmail.com>
2018-02-20 13:00:35 -0800

    added static to node server to load html

--- server/index.js
+++ server/index.js
@@ -1,35 +1,36 @@
-const express = require('express')
-const cors = require('cors')
-var bodyParser = require('body-parser')
+const express = require('express');
+const cors = require('cors');
+var bodyParser = require('body-parser');
 
-const utils = require('./utils')
+const utils = require('./utils');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../client/dist'));
 
-app.use(cors())
+app.use(cors());
 
 app.post('/items/:id', (req, res) => {
-  let rating = req.body.rating
-  let id = req.params.id
-  let resp = utils.write(id, rating)
-  if (resp) return res.send(resp)
-  return res.status(500).send('could not write result')
-})
+	let rating = req.body.rating;
+	let id = req.params.id;
+	let resp = utils.write(id, rating);
+	if (resp) return res.send(resp);
+	return res.status(500).send('could not write result');
+});
 
 app.get('/items', (req, res) => {
-  let amt = parseInt(req.query.amt) || 20
-  let page = parseInt(req.query.page) || 1
-  let resp = utils.read(amt, page)
-  if (resp) return res.send(resp)
-  return res.status(500).send('could not read results')
-})
+	let amt = parseInt(req.query.amt) || 20;
+	let page = parseInt(req.query.page) || 1;
+	let resp = utils.read(amt, page);
+	if (resp) return res.send(resp);
+	return res.status(500).send('could not read results');
+});
 
-app.listen('3000', (err) => {
-  if (err) {
-    console.log('there was a problem starting the server')
-  } else {
-    console.log('server listening port 3000')
-  }
-})
+app.listen('3000', err => {
+	if (err) {
+		console.log('there was a problem starting the server');
+	} else {
+		console.log('server listening port 3000');
+	}
+});
