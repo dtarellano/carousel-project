472bbb53654b976c9daa28241d49c62b7f686a13
David Arelano <dtarellano1@gmail.com>
2018-02-20 20:29:06 -0800

    successfully retrieving 16 sets of data

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
+let youtubeURL = 'https://www.youtube.com/watch?v=';
 export default class App extends Component {
 	state = {
 		hello: 'world'
 	};
+
+	componentDidMount() {
+		axios.get('/items/?p=1&amt=16').then(data => {
+			this.setState({
+				data: data.data
+			});
+		});
+	}
+
 	render() {
 		return (
 			<div>
