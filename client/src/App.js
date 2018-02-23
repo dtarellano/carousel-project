b8df99e40ae59c3e9255b42f494626e39efcda64
David Arelano <dtarellano1@gmail.com>
2018-02-23 13:41:44 -0800

    quality of life changes

--- client/src/App.js
+++ client/src/App.js
@@ -6,58 +6,62 @@ export default class App extends Component {
 	state = {
 		hello: 'world',
 		data: [],
-		display: []
+		display: [],
+		start
 	};
+
+	constructor() {
+		super();
+		const { data, display, start } = this.state;
+	}
 
 	componentDidMount() {
 		axios.get('/items/?page=1&amt=16').then(data => {
 			this.setState({
 				data: data.data,
-				display: [data.data[0], data.data[1], data.data[2], data.data[3]],
-        start: 0
+				display: [data.data[0], data.data[1], data.data[2], data.data[3]]
 			});
 		});
 	}
-	leftArrow() {
-    let start = this.state.start;
 
-    if (this.state.start <= 0) {
-      start = this.state.data.length - 4;
-      this.setState(state => {
-        let display = [];
-        for (let i = start; i < start + 4; i++) {
-          display.push(this.state.data[i]);
-        }
-        return {
-          display,
-          start: start
-        }
-      });
-    } else {
-      this.setState(state => {
-        let display = [];
-        start = start - 4;
-        for (let i = start; i < start + 4; i++) {
-          display.push(this.state.data[i]);
-        }
-        return {
-          display,
-          start: start
-        }
-      });
-    }
-  }
-	rightArrow() {
-    console.log('right arrow clicked')
-		if (this.state.start >= 15) {
-      this.setState({start: 0});
-    } else {
-      this.setState({start: this.state.start + 4});
-    }
+	leftArrow() {
+		let start = start;
+		if (start <= 0) {
+			start = data.length - 4;
+			this.setState(state => {
+				let display = [];
+				for (let i = start; i < start + 4; i++) {
+					display.push(data[i]);
+				}
+				return {
+					display,
+					start
+				};
+			});
+		} else {
+			this.setState(state => {
+				let display = [];
+				start = start - 4;
+				for (let i = start; i < start + 4; i++) {
+					display.push(data[i]);
+				}
+				return {
+					display,
+					start
+				};
+			});
+		}
 	}
-  // changeDisplay() {
-  //   if
-  // }
+
+	rightArrow() {
+		console.log('right arrow clicked');
+		if (this.state.start >= 15) {
+			this.setState({ start: 0 });
+		} else {
+			this.setState({ start: this.state.start + 4 });
+		}
+	}
+
 	render() {
 		let carousel = <div>Loading</div>;
 		if (this.state.data.length) {
@@ -69,7 +73,8 @@ export default class App extends Component {
 			<div>
 				<h3>Top recomendations for you</h3>
 				<div className="carousel">{carousel}</div>
-        <button onClick={() => this.leftArrow()}>Button</button>
+				<button onClick={() => this.leftArrow()}>Left</button>
+				<button onClick={() => this.rightArrow()}>Right</button>
 			</div>
 		);
 	}
